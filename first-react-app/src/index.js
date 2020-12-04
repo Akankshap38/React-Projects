dd11b4af6ee9504196aa37b7f0f36cbbeffdccf7
akanksha3008 <akanksha.pardeshi17@vit.edu>
2020-12-04 13:58:11 +0530

    Understanding of Import and export by creating a simple calculator

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Heading from './MyComponents';
-import {List} from './MyComponents';
-import { SecondList } from './MyComponents';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+
+import {add, divide, multiply, subtract} from './Components/calculate';
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
@@ -19,34 +13,12 @@ import { SecondList } from './MyComponents';
 reportWebVitals();
 
 
-// var d  = new Date();
-// console.log(d.getHours());
-// let content;
-// var customStyle ={color: ""};
- 
-// if(d.getHours()<=12)
-//   { content = "Good Morning";
-//     customStyle.color = "red";
-//   }
-// else if(d.getHours()<=18)
-//   { content = "Good Afternoon";
-//     customStyle.color = 'green';
-//   }
-  
-// else
-//   { content = "Good Evening";
-//     customStyle.color = "blue";
-//   }
-
-// function Heading(){
-//   return <h1>My Favourite Books</h1>
-// }
-
 ReactDOM.render(
   <div>
-    <Heading/>
-    <List/>
-    <SecondList/>
+    <li>{add(1,2)}</li>
+    <li>{subtract(2,3)}</li>
+    <li>{multiply(7,3)}</li>
+    <li>{divide(10,3)}</li>
   </div>
   
   ,document.getElementById("root")
